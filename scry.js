d8fc350af2a7cb3b98429e12b884654f72c1318c
Phillip Jones <pajonesvpbe@gmail.com>
2020-09-24 02:01:53 -0400

    Minor update. Began restructuring code.

--- scry.js
+++ scry.js
@@ -1,4 +1,5 @@
 const main = document.querySelector('main');
+const aside = document.querySelector('aside');
 
 window.addEventListener('load', function(){
     updateRandom();
@@ -7,23 +8,55 @@ window.addEventListener('load', function(){
       .catch(err => 'SW registration failed');
 });
 
-//gets a random card, yoinks the JSON, then displays it by calling displayCard
+//gets a random card, yoinks the JSON, then displays it by calling displayCardImg and displayCardInfo
 async function updateRandom(){
     const randomCard = await fetch(`https://api.scryfall.com/cards/random`);
     const cardJson = await randomCard.json();
 
-    main.innerHTML = displayCard(cardJson);
+    main.innerHTML = displayCardImg(cardJson);
+    aside.innerHTML = displayCardInfo(cardJson);
 }
 
+/*
+Deprecated. Use displayCardImg and displayCardInfo instead
+*/
 function displayCard(cardJson) {
+    var statsLine;
+    if (cardJson.power == undefined && cardJson.toughness == undefined) {
+        statsLine = "";
+    } else {
+        statsLine = cardJson.power + " / " + cardJson.toughness;
+    }
     return `
       <div>
-          <img src="${cardJson.image_uris.normal}" style="max-width:100%;height:auto;" alt="${cardJson.name}">
+          <img src="${cardJson.image_uris.normal}"  alt="${cardJson.name}">
           <h2>${cardJson.name} -- ${cardJson.mana_cost}</h2>
           <p>${cardJson.oracle_text}</p>
-          <p>${cardJson.power} / ${cardJson.toughness}</p>
+          <p>${statsLine}</p>
           <p>Art by ${cardJson.artist}</p>
       </div>
+    `;
+}
+
+function displayCardImg(cardJson) {
+    return `
+        <div>
+        <img src="${cardJson.image_uris.normal}" style="max-width:100%;height:auto;" alt="${cardJson.name}">
+        </div>
+    `;
+}
+
+function displayCardInfo(cardJson) {
+    return `
+        <div class="panel panel-default">
+        <div class="panel-heading">${cardJson.name}  ${cardJson.mana_cost}</div>
+        <div class="panel-body">
+            <p>${cardJson.type_line}</p>
+            <p>${cardJson.oracle_text}</p>
+            <p id="stats">${cardJson.power} / ${cardJson.toughness}</p>
+        </div>
+        <div class="panel-footer">Art by ${cardJson.artist}</div>
+        </div>
     `;
 }
 
